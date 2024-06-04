e02f1ab9ba83ff29fc79b3045f0b8e177f5db980
Jack Summerfield <summerzj10@gmail.com>
2024-06-04 11:08:25 +0000

    feat: add all function names + ensure btn clicked is int

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,100 @@ let btn = document.getElementsByClassName('btn');
 for (object of btn) {
     object.addEventListener('click', function(e) {
         let btnValue = e.target.textContent;
-        display.value += btnValue;
+        if (!isNaN(btnValue) || btnValue === '.') {
+            display.value += btnValue;
+        }
     })
+}
+
+function division() {
+
+}
+
+function multiply() {
+    
+}
+
+function subtract() {
+    
+}
+
+function addition() {
+    
+}
+
+function equal() {
+    
+}
+
+function percent() {
+    
+}
+
+function plusMinus() {
+    
+}
+
+function clear() {
+    
+}
+
+function factorial() {
+    
+}
+
+function openBrace() {
+    
+}
+
+function closedBrace() {
+    
+}
+
+function xPowTwo() {
+    
+}
+
+function xPowThree() {
+    
+}
+
+function xPowY() {
+    
+}
+
+function xSqrtTwo() {
+    
+}
+
+function xSqrtThree() {
+    
+}
+
+function xSqrtY() {
+    
+}
+
+function sin() {
+    
+}
+
+function cos() {
+    
+}
+
+function tan() {
+    
+}
+
+function log() {
+    
+}
+
+function e() {
+    
+}
+
+function pi() {
+    
 }
